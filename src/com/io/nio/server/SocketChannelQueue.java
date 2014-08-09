bc11cd04e5ee7f7b97d448ef72ef742e96246d34
Administrator <Administrator@MSDN-SPECIAL>
2014-08-09 14:55:29 +0900

    add io/nio

---
+++ src/com/io/nio/server/SocketChannelQueue.java
@@ -0,0 +1,31 @@
+package com.io.nio.server;
+
+
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.LinkedList;
+
+
+public class SocketChannelQueue {
+        
+        private LinkedList list = new LinkedList();
+        private Selector readSelector;
+        
+        SocketChannelQueue() {
+        }
+        
+        public void setReadSelector(Selector readSelector) {
+                this.readSelector = readSelector;
+        }
+        
+        public void addLast(SocketChannel channel) {
+                list.addLast(channel);
+                readSelector.wakeup();
+        }
+        
+        public SocketChannel getFirst() {
+                if (list.size() == 0) return null;
+                
+                return (SocketChannel)list.removeFirst();
+        }
+}
